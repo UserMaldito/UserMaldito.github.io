5b9c48e5125f12f00285026f91479d4ad1c3f3f9
Aday Marfil (Alumnado) <adaymarfil89@gmail.com>
2024-12-04 10:02:58 +0100

    Demasiado código y dolor de cabeza

--- DEW/UT04-01/script.js
+++ DEW/UT04-01/script.js
@@ -2,63 +2,119 @@ const DOM = {
     formulario: document.forms[0],
     nombreUsuario: document.getElementById("NombreUsuario"),
     contrasena: document.getElementById("Contrasena"),
+    mostrarContrasena: document.getElementById("show"),
     nombreReal: document.getElementById("Nombre"),
     apellidos: document.getElementById("Apellidos"),
     telefono: document.getElementById("Telefono"),
     codigoPostal: document.getElementById("CodigoPostal"),
     tipoDocumento: document.getElementById("TipoDocumento"),
-    cuentaComo: document.getElementById("CuentaComo"),
+    valorDocumento: document.getElementById("DniNie"),
+    cuentaComo: document.getElementsByName("CuentaComo"),
     anioNacimiento: document.getElementById("AnioNacimiento"),
     aficiones: document.getElementById("Aficiones"),
-    aficionesCheckbox: document.querySelectorAll("[type=checkbox]"),
+    aficionesCheckbox: document.querySelectorAll("#section-2 [type=checkbox]"),
     publicacionTitulo: document.getElementById("PublicacionTitulo"),
     publicacionDescripcion: document.getElementById("PublicacionDescripcion"),
     fieldsetError: document.querySelectorAll("fieldset")[1]
 };
 
-let RatificandoContrasena = () => {
-    if (DOM.contrasena.textContent.trim().length == 8) {
-        return null;
-    }
+let ErrorObjeto = {
+    campo,
+    mensaje: ""
 };
-//Valida: NombreUsuario, Nombre, Apellido, AnioNacimiento, PublicacionTitulo y PublicacionDescripcion.
-let RatificandoDatos = (campo, nombre, validationMessage) => {
-    // if (!(datoCampo >= min) && (datoCampo <= max)) {
-
+//Valida: NombreUsuario, Nombre, Apellido, AnioNacimiento, Telefono, Contraseña, CodigoPostal, PublicacionTitulo y PublicacionDescripcion.
+let RatificandoDatos = (campo) => {
     if (!campo.validity.valid) {
-        if (validationMessage == "") {
-            validationMessage = "Dato Inválio. Debe entar dentro del rango establecido: Min: " + min + " / Max: " + max;
+        ErrorObjeto.campo = campo;
+        ErrorObjeto.mensaje = "Error Campo " + campo.name + ": " + campo.validationMessage;
+        throw (ErrorObjeto);
+    }
+    return true;
+};
+
+let RatificandoDNINIE = (campoDocumento, valorDocumento) => {
+    let dniCorrecto = RatificandoDatos(valorDocumento);
+    if (dniCorrecto) {
+        const ARRAY_LETRAS_DNI = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
+
+        let digitoControl = 0;
+        var numeroDni = valorDocumento.value.slice(0, valorDocumento.value.length - 1);
+
+        if (campoDocumento.value == "NIE") {
+            let primeraLetraDni = valorDocumento.value.slice(0, 1).toUpperCase();
+            switch (primeraLetraDni) {
+                case "X":
+                    digitoControl = 0;
+                    break;
+                case "Y":
+                    digitoControl = 1;
+                    break;
+                case "Z":
+                    digitoControl = 2;
+                    break;            
+                default:
+                    ErrorObjeto.campo = campo;
+                    ErrorObjeto.mensaje = "Error Campo " + campoDocumento.name + ": " + "Primera Letra del NIE No Válido";
+                    throw (ErrorObjeto);
+            }
+            
+            numeroDni = valorDocumento.value
+            .replace(valorDocumento.value[0], digitoControl)
+            .slice(0, valorDocumento.value.length - 1);
         }
-        throw ("Error campo " + nombre + ": " + validationMessage);
+
+        let letraDNI = valorDocumento.value.slice(valorDocumento.value.length - 1, valorDocumento.value.length).toUpperCase();
+        let restoNumeroDni = (numeroDni) % 23;
+        let letraCalculada = ARRAY_LETRAS_DNI[restoNumeroDni];
+
+    if (letraCalculada != letraDNI) {
+            ErrorObjeto.campo = campo;
+            ErrorObjeto.mensaje = "Error Campo " + campoDocumento.name + ": " + "DNI/NIE No Válido";
+            throw (ErrorObjeto);
+        }
     }
 };
+let RatificandoCuenta = (campo) => {
+    let tipoCuenta = "";
+    
+    campo.forEach((cuenta) => {
+        if (cuenta.checked) {
+            tipoCuenta = cuenta.value;
+        }
+    });
 
-//Valida: Telefono, Contraseña y CodigoPostal
-let RatificandoPattern = (campo, regex) => {
-    if (campo.value.trim().match(regex)) {
-        return true;
+    if (tipoCuenta == "") {
+        ErrorObjeto.campo = campo;
+        ErrorObjeto.mensaje = "Error Campo " + campo.name + ": Debe elegir al menos 1 Tipo de Cuenta";
+        throw (ErrorObjeto);
     }
-    let mensaje = "Dato Inválio. No cumple con los requisitos...";
-    throw ("Error campo " + campo.name + ": " + campo.validationMessage);
-};
-let RatificandoDNINIE = (campo, regex) => {
 
+    return tipoCuenta;
 };
-//Valida: CuentaComo, TipoDocumento
-let RatificandoCampoVacio = (campo) => {
-    if (campo.value.trim().length == 0) {
-        throw ("Error campo " + campo.name + ": " + campo.validationMessage);
+
+let VerificandoAficiones = (checkboxArray, campo) => {
+    let aficiones = Array
+            .from(checkboxArray)
+            .filter(checkbox => checkbox.checked)
+            .map(checkbox => checkbox.value.toString());
+
+    if (aficiones.length < 2) {
+        ErrorObjeto.campo = campo;
+        ErrorObjeto.mensaje = "Error Campo " + campo.name + ": Debe seleccionar un mínimo de 2 " + campo.name;
+        throw (ErrorObjeto);
     }
-};
-let RatificandoAficiones = () => {
 
+    return JSON.stringify(aficiones);
 };
-let MostrarError = (evento, mensaje) => {
-    evento.preventDefault();
-
+let MostrarErrorPersonal = (mensaje) => {
     let cajaError = document.createElement("span");
     cajaError.textContent = mensaje;
     DOM.fieldsetError.appendChild(cajaError);
+};
+let MostrarErrorDefault = (objetoError) => {
+    let cajaError = document.createElement("span");
+    cajaError.textContent = mensaje;
+    objetoError.campo.insertAdjacentElement("afterend", cajaError);
 };
 let LimpiarError = () => {
     DOM.fieldsetError.childNodes.forEach((nodo) => nodo.remove());
@@ -66,47 +122,52 @@ let LimpiarError = () => {
 
 let ValidadorFormulario = (e) => {
     LimpiarError();
-
-    const MIN_STRING = 4;
-    const MIN_ANIO = 1920;
-    const MAX_LENGH_TITULO = 15;
-    const MAX_STRING_USER_DATA = 20;
-    const MAX_LENGH_PUBLICACION = 120;
-    const MAX_ANIO = 2010;
-
-    const REGEX_TELEFONO = "/\((\+[\d]{1,3})\)([\d]{9})/gmA";
-    const REGEX_PASSWORD = "/[\d]{8}/gmA";
-    const REGEX_CODIGO_POSTAL = "/38[\d]{3}/gmA";
-
-
+    
     try {
-        let nombreUsuario = RatificandoDatos(DOM.nombreUsuario, DOM.nombreUsuario.id, DOM.nombreUsuario.validationMessage);
-        let contrasena =  RatificandoPattern(DOM.contrasena.value, REGEX_PASSWORD);
-        let nombreReal =  RatificandoDatos(DOM.nombreReal.value.trim().length, MIN_STRING, MAX_STRING_USER_DATA, DOM.contrasena.id, DOM.contrasena.validationMessage);
-        let apellidos =  RatificandoDatos(DOM.apellidos.value.trim().length, MIN_STRING, MAX_STRING_USER_DATA, DOM.apellidos.id, DOM.apellidos.validationMessage);
-        let telefono = RatificandoPattern(DOM.telefono.value, REGEX_TELEFONO);
-        let codigoPostal =  RatificandoPattern(DOM.codigoPostal.value, REGEX_CODIGO_POSTAL);
+        RatificandoDatos(DOM.nombreUsuario);
+        RatificandoDatos(DOM.contrasena);
+        RatificandoDatos(DOM.nombreReal);
+        RatificandoDatos(DOM.apellidos);
+        RatificandoDatos(DOM.telefono);
+        RatificandoDatos(DOM.codigoPostal);
         
-        let tipoDocumento = DOM.tipoDocumento.value;
-        let cuentaComo = DOM.cuentaComo.value;
-        
-        let anioNacimiento = RatificandoDatos(DOM.anioNacimiento.value.trim(), MIN_ANIO, MAX_ANIO, DOM.anioNacimiento.id, DOM.anioNacimiento.validationMessage);
-        
-        let aficiones = Array
-            .from(DOM.aficionesCheckbox)
-            .filter(checkbox => checkbox.checked)
-            .map(checkbox => checkbox.value);
-        
-        let publicacionTitulo = RatificandoDatos(DOM.publicacionTitulo.value.trim().length, MIN_STRING, MAX_LENGH_TITULO, DOM.publicacionTitulo.id, DOM.publicacionTitulo.validationMessage);
-        let publicacionDescripcion = RatificandoDatos(DOM.publicacionDescripcion.value.trim().length, MIN_STRING, MAX_LENGH_PUBLICACION, DOM.publicacionDescripcion.id, DOM.publicacionDescripcion.validationMessage);
-    } catch (error) {
-        MostrarError(e, error);
-    }
+        RatificandoDNINIE(DOM.tipoDocumento, DOM.valorDocumento)
 
+        RatificandoCuenta(DOM.cuentaComo);
+        RatificandoDatos(DOM.anioNacimiento);
+        
+        DOM.aficiones.value = VerificandoAficiones(DOM.aficionesCheckbox, DOM.aficiones)
+        
+        RatificandoDatos(DOM.publicacionTitulo);
+        RatificandoDatos(DOM.publicacionDescripcion);
+    } catch (errorObjeto) {
+        evento.preventDefault();
+
+        MostrarErrorPersonal(errorObjeto);
+        MostrarErrorDefault(errorObjeto);
+    }
 };
 
-(function () {
-let x = 9;
+(() => {
     DOM.formulario.addEventListener("submit", (e) => {ValidadorFormulario(e)});
+
+    //Verifica si, al cargar el formulario, se muestra la contraseña
+    DOM.contrasena.type = DOM.mostrarContrasena.checked? "text" : "password";
+    //Luego ya le digo que cambie de tipo si detecta cambio
+    DOM.mostrarContrasena.addEventListener("change", () => {
+        DOM.contrasena.type = DOM.mostrarContrasena.checked? "text" : "password";
+    });
+    
 }
 )();
+
+/*
+    Falta:
+    - Desuso del Br en el HTML
+    - Parte Izq. mensaje por defecto / Parte Der. mensaje personalizado
+    - Cambiar ValorDocumento por DniNie
+    - Implementar número de caracteres introducidos en los input finales
+    - Implementar cambio de Estado (de inválido a válido) en todos los campos
+    - Uso modular del ESM
+    - 
+*/
